3908efe2ea1a29e22455930ff13fbd2c0578c56c
Altay02 <Busurmankulov.altay@mail.ru>
2021-12-16 05:06:40 +0300

    Update makeCV.sh

--- Simple-CV/makeCV.sh
+++ Simple-CV/makeCV.sh
@@ -3,3 +3,5 @@ pdflatex -interaction=nonstopmode -halt-on-error Simple-CV.tex
 ls -lah
 cp Simple-CV.pdf resume.pdf
 ls -lah
+git config --global user.email "busurmankulov.altay@mail.ru"
+git config --global user.name "Altay02"
