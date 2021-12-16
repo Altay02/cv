b438ab7ff59d600f0ddd7ca794e92e3581e83ae9
Altay02 <Busurmankulov.altay@mail.ru>
2021-12-16 03:46:30 +0300

    Update copy-pdf.sh

--- Simple-CV/copy-pdf.sh
+++ Simple-CV/copy-pdf.sh
@@ -2,15 +2,15 @@
 ls -lah
 git config --global user.email "busurmankulov.altay@mail.ru"
 git config --global user.name "Altay02"
-git remote -v
-git remote set-url origin mySSH_url
-git clone ssh://git@github.com:Altay02/cv.git
+#git remote -v
+#git remote set-url origin mySSH_url
+#git clone ssh://git@github.com:Altay02/cv.git
 ls -lah
 cd cv
 ls -lah
 cp Simple-CV.pdf resume.pdf
 ls -lah
-git add -A
-git status
-git commit -m "update resume"
-git push origin main:gh-pages
+#git add -A
+#git status
+#git commit -m "update resume"
+#git push origin main:gh-pages
