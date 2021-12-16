1af16e04127e27d0cbe4d79c265be8b4b0ee6856
Altay02 <Busurmankulov.altay@mail.ru>
2021-12-16 05:17:26 +0300

    Update makeCV.sh

--- Simple-CV/makeCV.sh
+++ Simple-CV/makeCV.sh
@@ -1,5 +1,5 @@
 ls -lah
-pdflatex -interaction=nonstopmode -halt-on-error Simple-CV.tex
+pdflatex -interaction=nonstopmode -halt-on-error /Simple-CV.tex
 ls -lah
 cp /Simple-CV.pdf /resume.pdf
 ls -lah
